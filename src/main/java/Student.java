06de67b82da714562509403c1b7658c6f7bda9d1
Nicolai Kopka <nicolai.kopka@aol.de>
2022-05-23 12:42:08 +0200

    initial submit

---
+++ src/main/java/Student.java
@@ -0,0 +1,29 @@
+class Student {
+    private String name;
+    private int id;
+    private String email;
+
+    String getName() {
+        return name;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    String getEmail() {
+        return email;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    void setEmail(String email) {
+        this.email = email;
+    }
+}
